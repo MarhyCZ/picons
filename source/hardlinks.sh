42c7dfcc95750ba187259ce9b6562e6f9718f4e3
Michal Marhan <marhanmichal@gmail.com>
2019-11-10 21:42:25 +0100

    Love Nature

--- source/hardlinks.sh
+++ source/hardlinks.sh
@@ -179,6 +179,7 @@ ln hbo.png hbohd.png
 ln hbo2.png hbo2hd.png
 ln hbo3.png hbo3hd.png
 ln history.png historyhd.png
+ln lovenature.png lovenaturehd.png
 ln stingrayiconcerts.png iconcerts.png
 ln insighttv.png insighttvhdint.png
 ln nationalgeographic.png ngc.png
